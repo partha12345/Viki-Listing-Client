e83f577106040569db119d74b3601c22674e7a98
parth12345 <csksarathi07@gmail.com>
2023-08-15 11:31:51 +0530

    15

--- client/src/collections/Header/Header.js
+++ client/src/collections/Header/Header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Header.css'
+import favicon from '../../assets/Images/Group 158 1.png'
 
 const Header = () => {
   return (
-    <div className='Header'>
-                       
-    </div>    
+    <header className='Header'>
+            <nav className='navbar navbar-expand-lg'>
+                <div className='container-fluid'>
+                    <a className='navbar-brand' href='#'>
+                        <img src={favicon}></img>
+                    </a>
+                </div>
+                </nav>   
+    </header>    
   )
 }
 
