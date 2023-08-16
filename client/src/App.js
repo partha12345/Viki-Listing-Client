a444e1e4eb8e77b247de8d0aa6745ab47379b26f
Venaikishore@5 <kishorevenai@gmail.com>
2023-08-16 18:04:46 +0530

    Kishore

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import NewsList from './pages/Articles/NewsList';
 import Layout from './collections/Layout';
 import Home from './pages/Home/Home';
+import About from './pages/About/About';
 
 
 function App() {
   return (
     <Routes>
        <Route path='/' element={<Layout/>}>
-         <Route index element={<Home/>}></Route>                           
+         <Route index element={<Home/>}/>
+         <Route path='about' element={<About/>}></Route>
        </Route>      
     </Routes>
   );
---
+++ client/src/pages/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className='About'>    
+       <h2>About</h2> 
+       <img></img> 
+       <p>An import-export business plan is the first step in starting your business. A business plan acts as a roadmap for your future endeavours of the company. A business plan consists of a financial plan, management plan, daily expenditures of a business, import and export costs, warehousing costs, location charges, transportation cost, labour cost, etc.</p>
+       <p>The next step is to choose a location for your export firm. A location must be accessible to all the export ports and airports. You need to find a location that best suits your business and customers' requirements. If your firm is accessible to ports, it will help you reduce your transportation costs. A location must be in a good area where you can store your inventories and gold stocks. Opening a firm in a small locality might not be a good idea because it can be risky. You can open a firm where middle-class and rich people reside. Usually, those areas are near airports or ports. It is essential to choose a location that is safe for your business</p>
+       <div>
+        <p>The longer I live, the more I realize that I am never wrong about anything, and that all the pains I have so humbly taken to verify my notions have only wasted my time!</p>
+       </div>
+       <p>After you set up your business location, you need a business bank account. You need a bank account that is authorised and permitted by the government to deal in foreign exchange. The account will be registered with the Customs and issued by an Authorised Dealer (AD). To open an account, you need a name for your business firm. The documents required to open an account include a photocopy of a PAN, a photocopy of ration card, recent passport photographs, etc.</p>
+       <p>after setting up your export gold jewellery business, you will need to find suppliers and buyers for your business. You need to find suppliers who are reliable and provide you with the best quality gold. You can participate in the trade fair, buyer-seller meets, exhibitions and B2B portals to help find suppliers and buyers. You can also use social media platforms to find buyers. Your target audience is your buyer. So, it is essential to promote your company to your selective target audience.</p>
+       <img></img>
+       <p>The next step is to start your manufacturing operations. After you find your suppliers and buyers, you need to procure an export order. You can send samples or a proforma invoice. Once you do it, you will receive orders. You can either buy goods from manufacturers or can establish your manufacturing process. It is also essential to perform quality checks before sending the shipment. The shipment will be checked and scrutinised at every stage of exporting. </p>
+       <p>The gold export business involves careful management of the accounts. It is a high-risk business, so you need to streamline the accounting process. You can use the OKCredit app to track and manage the accounting transactions of the business.You can also hire a Clearing House Agent (CHA) for shipment bills. After clearance, your goods are ready to export. Once your shipment is exported, you will receive the payment for the shipment.</p>
+    </div>
+  )
+}
+
+export default About
