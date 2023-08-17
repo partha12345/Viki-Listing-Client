27477d4be58e5920bb86c02954de5dc4f49efeaf
parth12345 <csksarathi07@gmail.com>
2023-08-17 10:41:04 +0530

    Partha

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,6 +2,13 @@ import React from 'react'
 import './Home.css'
 import { useGetArticleQuery } from '../../features/News/newsApiSlice'
 import HomeBottomContent from './HomeBottomContent'
+import favimagesone from  '../../assets/Images/1.png'
+import favimagestwo from  '../../assets/Images/2.png'
+import favimagesthree from  '../../assets/Images/3.png'
+import favimagesfour from  '../../assets/Images/4.png'
+import favimagesfive from  '../../assets/Images/5.png'
+
+
 
 
 
@@ -42,7 +49,7 @@ const Home = () => {
   <div className='news-part1-sidenews'>
 
               <div className='newsContents'>
-                 <img className='news-images-small' alt='news-image'></img>
+                 <img className='news-images-small' src={{favimagesone}} style={{width:50}} alt='news-image'></img>
                  <div className='newsData'>
                      <h4>coordination between the business</h4>
                      <p2>It has earned the reputation of being the chosen electrical goods</p2>
@@ -50,7 +57,7 @@ const Home = () => {
                  </div>                   
               </div>
               <div className='newsContents'>
-                 <img className='news-images-small' alt='news-image'></img>
+                 <img className='news-images-small'  src={{favimagestwo}} style={{width:50}} alt='news-image'></img>
                  <div className='newsData'>
                      <h4>greenhouse gases that affect environment</h4>
                      <p2>It has earned the reputation of being the chosen electrical goods</p2>
@@ -58,7 +65,7 @@ const Home = () => {
                  </div>                   
               </div>
               <div className='newsContents'>
-                 <img className='news-images-small' alt='news-image'></img>
+                 <img className='news-images-small' src={{favimagesthree}} style={{width:50}} alt='news-image'></img>
                  <div className='newsData'>
                      <h4>Some stores charge a small amount.</h4>
                      <p2>It has earned the reputation of being the chosen electrical goods</p2>
@@ -66,7 +73,7 @@ const Home = () => {
                  </div>                   
               </div>
               <div className='newsContents'>
-                 <img className='news-images-small' alt='news-image'></img>
+                 <img className='news-images-small' src={{favimagesfour}} style={{width:50}} alt='news-image'></img>
                  <div className='newsData'>
                      <h4>Friends and Family Shave</h4>
                      <p2>It has earned the reputation of being the chosen electrical goods</p2>
@@ -74,7 +81,7 @@ const Home = () => {
                  </div>                   
               </div>
               <div className='newsContents'>
-                 <img className='news-images-small' alt='news-image'></img>
+                 <img className='news-images-small' src={{favimagesfive}} style={{width:50}} alt='news-image'></img>
                  <div className='newsData'>
                      <h4>Person Shares Satellite Images</h4>
                      <p2>It has earned the reputation of being the chosen electrical goods</p2>
@@ -85,13 +92,7 @@ const Home = () => {
 
 
 
-          <section className='mobile-part'>
-            
-          <div className='mobile-news'>
-                 <h3>Man buys fake Puma shoes from local market. Swiggy instamart's reply to his post</h3>
-                 <p5>Most of us are guilty of buying cheap first copies of high-end brands just to show it off to our friends. Well, sometimes that actually helped us grab the spotlight.</p5>
-               </div>
-          </section>
+   
 
           
            <div className='part1-middlenews'>
@@ -115,7 +116,14 @@ const Home = () => {
                <img></img>
                <img></img>
            </div>
-        </section>            
+        </section>  
+        <section className='mobile-part'>
+            
+            <div className='mobile-news'>
+                   <h3>Man buys fake Puma shoes from local market. Swiggy instamart's reply to his post</h3>
+                   <p5>Most of us are guilty of buying cheap first copies of high-end brands just to show it off to our friends. Well, sometimes that actually helped us grab the spotlight.</p5>
+                 </div>
+            </section>          
 
     </>
   
