f7a41f2cfdd665437a5acb9a0644ef62b7db6584
parth12345 <csksarathi07@gmail.com>
2023-08-22 17:47:19 +0530

    Partha

--- src/collections/Header/Header.js
+++ src/collections/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Header.css'
 import favicon from '../../assets/Images/Group 158 1.png'
+import { Link } from 'react-bootstrap/lib/Navbar'
 
 
 
@@ -51,7 +52,8 @@ const Header = () => {
                 </div>
                
                 <div class="category">
-                    <a class="btn btn-dark rounded-5" href="#gridcontainer" role="button">Category</a>
+                    {/* <a class="btn btn-dark rounded-5" href="#gridcontainer" role="button">Category</a> */}
+                    <Link to = '/category'>Category</Link>
                 </div>
                 </div>
                 </div>
